650158487acdbe82e7cc26f0b776346a0b3db4b2
zhan <z1764482801@163.com>
2020-12-02 23:28:00 +0800

    23:27 可是简单的获取token 调取接口

--- src/api/login.js
+++ src/api/login.js
@@ -1,8 +1,17 @@
 import request from '@/assets/js/request'
+// 登录接口
 export function login (data) {
     return request({
         url: '/api/private/v1/login',
         method: 'post',
         data
+    })
+}
+// 用户数据列表
+export function usersList (params) {
+    return request({
+        url: '/api/private/v1/users',
+        method: 'get',
+        params
     })
 } 
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -2,55 +2,87 @@
   <div class="Login">
     <div class="dialog">
       <el-form
+        ref="ruleForm"
         :model="ruleForm"
         :rules="rules"
-        ref="ruleForm"
         label-width="100px"
-        class="demo-ruleForm"
       >
-        <el-form-item label="用户名" prop="username">
-          <el-input v-model="ruleForm.username"></el-input>
+        <el-form-item label="用户名" 
+        prop="username">
+          <el-input
+            placeholder="请输入账号"
+            v-model="ruleForm.username"
+          ></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="password">
-          <el-input v-model="ruleForm.password"></el-input>
+          <el-input
+            placeholder="请输入密码"
+            v-model="ruleForm.password"
+            type="password"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submit">提交</el-button>
         </el-form-item>
       </el-form>
     </div>
   </div>
 </template>
 <script>
+import { login ,usersList} from "@/api/login.js";
 export default {
   name: "Login",
   components: {},
   data() {
     return {
       ruleForm: {
-        username: "",
-        password: ""
+        username: "admin",
+        password: "123456",
       },
       rules: {
         username: [
           {
             required: true,
             message: "请输入账号",
-            trigger: "change"
-          }
+            trigger: "change",
+          },
         ],
         password: [
           {
             required: true,
             message: "请输入账号",
-            trigger: "change"
-          }
-        ]
-      }
+            trigger: "change",
+          },
+        ],
+      },
     };
   },
   created() {},
   computed: {},
-  methods: {},
+  methods: {
+    submit() {
+      this.$refs["ruleForm"].validate((valid) => {
+        if (!valid) return false;
+        login(this.ruleForm).then((res) => {
+          // console.log(res);
+          localStorage.setItem('token', res.data.token)
+          this.usersList()
+        });
+      });
+    },
+    usersList(){
+      let params = {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      }
+      usersList(params).then((res)=> {
+        console.log(res)
+      })
+    }
+  },
   mounted() {},
-  activated() {} // 如果页面有keep-alive缓存功能，这个函数会触发
+  activated() {}, // 如果页面有keep-alive缓存功能，这个函数会触发
 };
 </script>
 <style lang="scss" scoped>
